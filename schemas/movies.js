bd48fffbf9c92b8813417ab6243d5d499a10dd31
Pau Velasco <pauvelascogarrofe@gmail.com>
2024-02-13 18:48:50 +0100

    Add initial project files and configurations

---
+++ schemas/movies.js
@@ -0,0 +1,36 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required'
+
+  }),
+  year: z.number().int().min(1900).max(2025),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10),
+  poster: z.string().url({
+    message: 'Poster must be a valid url'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi', 'Crime']),
+    {
+      required_error: 'Genre is required'
+    }
+  )
+
+})
+
+function validateMovie (input) {
+  return movieSchema.safeParse(input)
+}
+
+function validatePartialMovie (input) {
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
